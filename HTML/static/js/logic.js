2535469855e63ab973e690de19715463eed96b13
catsdata <96197979+catsdata@users.noreply.github.com>
2022-06-02 23:32:19 -0400

    logic backup

--- HTML/static/js/logic.js
+++ HTML/static/js/logic.js
@@ -19,7 +19,7 @@ let darkstreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/
 
 // map object with center for Charlotte NC, zoom level for Charlotte and surrounding counties and default tile layer.
 let map = L.map('mapid', {
-	center: [35.2271, -80.8431],
+	center: [35.2845, -80.6688],
 	zoom: 9,
 	layers: [streets]
 });
@@ -65,57 +65,84 @@ d3.json("https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/
   }).addTo(zips);
   
   zips.addTo(map);
-  });
+});
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+// Create Girl Scout Logo markers
+
+// Define icon class for shared options
+var gsIcon = L.Icon.extend({
+  options: {
+      iconSize:     [20, 20],
+      iconAnchor:   [20, 20],
+      popupAnchor:  [0, -50]
+  }
+});
+
+// Define seperate color icons with URL's
+var greenIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/green.png'}),
+    redIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/red.png'}),
+    orangeIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/orange.png'}),
+    yellowIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/yellow.png'}),
+    blueIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/blue.png'}),
+    purpleIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/purple.png'}),
+    pinkIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/pink.png'}),
+    greyIcon = new gsIcon({iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/grey.png'})
+    ;
+console.log(greenIcon)
+
+// test one marker
+
+// var greenTestIcon = L.icon({
+//   iconUrl: 'https://raw.githubusercontent.com/sbooysen/Final-Project-Data/catshtml/Images/logos/green.png',
+//   iconSize:     [20, 20], // size of the icon
+//   iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
+//   popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+// });
+
+// L.marker([35.2845, -80.6688], {icon: greenTestIcon}).addTo(map);
+
 // Create All Sales overlay
+var salesURL = "https://raw.githubusercontent.com/catsdata/catsdata.github.io/main/tempcook/sales.geojson"
 
 // Bring in the cookie GeoJSON data.
-d3.json("https://raw.githubusercontent.com/catsdata/catsdata.github.io/main/tempcook/sales.geojson").then(function(data) {
+d3.json(salesURL).then(function(data) {
 
   function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.allsales),
-      color: "#000000",
-      radius: 10,
-      stroke: true,
-      weight: 0.5
+    return {icon: getLogo(feature.properties.allsales)
     };
   }
 
-  // This function determines the color of the marker based on the quanity of cookie sales.
-  function getColor(allsales) {
+  function getLogo(allsales) {
     if (allsales >= 40000) {
-      return "#ff0000";
+      return redIcon;
     }
     if (allsales >= 30000) {
-      return "#ffa500";
+      return orangeIcon;
     }
     if (allsales >= 20000) {
-      return "#ffff00";
+      return yellowIcon;
     }
     if (allsales >= 10000) {
-      return "#008000";
+      return greenIcon;
     }
     if (allsales >= 5000) {
-      return "#0000ff";
+      return blueIcon;
     }
     if (allsales >= 2000) {
-      return "#4b0082";
+      return purpleIcon;
     }
     if (allsales >= 500) {
-      return "#ee82ee";
+      return pinkIcon;
     }
-    return "#f5f5f5"
+    return greyIcon
   };
+  console.log(getLogo)
 
   L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
-        console.log(data);
-        return L.circleMarker(latlng);
+      return gsIcon(latlng);
       },
     style: styleInfo,
     onEachFeature: function(feature, layer) {
@@ -127,15 +154,74 @@ d3.json("https://raw.githubusercontent.com/catsdata/catsdata.github.io/main/temp
         "<br>2019 Boxes Sold: " + feature.properties.sales2019 + 
         "<br>2020 Boxes Sold: " + feature.properties.sales2020 + 
         "<br>2021 Boxes Sold: " + feature.properties.sales2021 + 
-        "<br>2022 Boxes Sold: " + feature.properties.sales2022);
-  }
-}).addTo(allcookiesales);
+        "<br>2022 Boxes Sold: " + feature.properties.sales2022).openPopup();
+    }
+  }).addTo(allcookiesales);
 
 allcookiesales.addTo(map);
 });
 
+  // function styleInfo(feature) {
+  //   return {
+  //     opacity: 1,
+  //     fillOpacity: 1,
+  //     fillColor: getColor(feature.properties.allsales),
+  //     color: "#000000",
+  //     radius: 10,
+  //     stroke: true,
+  //     weight: 0.5
+  //   };
+  // }
+
+  // // This function determines the color of the marker based on the quanity of cookie sales.
+  // function getColor(allsales) {
+  //   if (allsales >= 40000) {
+  //     return "#ff0000";
+  //   }
+  //   if (allsales >= 30000) {
+  //     return "#ffa500";
+  //   }
+  //   if (allsales >= 20000) {
+  //     return "#ffff00";
+  //   }
+  //   if (allsales >= 10000) {
+  //     return "#008000";
+  //   }
+  //   if (allsales >= 5000) {
+  //     return "#0000ff";
+  //   }
+  //   if (allsales >= 2000) {
+  //     return "#4b0082";
+  //   }
+  //   if (allsales >= 500) {
+  //     return "#ee82ee";
+  //   }
+  //   return "#f5f5f5"
+  // };
+
+  // L.geoJson(data, {
+  //   pointToLayer: function(feature, latlng) {
+  //       console.log(data);
+  //       return L.circleMarker(latlng);
+  //     },
+  //   style: styleInfo,
+  //   onEachFeature: function(feature, layer) {
+  //     layer.bindPopup(
+  //       "Zip Code: " + feature.properties.zip + 
+  //       "<br>Primary Service Unit: " + feature.properties.serviceunitname + 
+  //       "<br>Median Income: $" + feature.properties.medianincome + 
+  //       "<br>Total Boxes Sold: " + feature.properties.allsales + 
+  //       "<br>2019 Boxes Sold: " + feature.properties.sales2019 + 
+  //       "<br>2020 Boxes Sold: " + feature.properties.sales2020 + 
+  //       "<br>2021 Boxes Sold: " + feature.properties.sales2021 + 
+  //       "<br>2022 Boxes Sold: " + feature.properties.sales2022);
+  // }
+// }).addTo(allcookiesales);
+
+// allcookiesales.addTo(map);
+// });
+
 // LEGEND
-// NEED TO FLIP LEGEND CODE
 
 let legend = L.control({
   position: "bottomright"
@@ -146,14 +232,14 @@ legend.onAdd = function() {
 
   const sales = ['0', '500', '2k', '5k', '10k', '20k', '30k', '40k'];
   const colors = [
-    "#D3D3D3",
-    "#ee82ee",
-    "#4b0082",
-    "#0000ff",
-    "#008000",
-    "#ffff00",
-    "#ffa500",
-    "#ff0000"
+    "#a8a8a8",
+    "#fd329e",
+    "#9e5fd6",
+    "#1496d4",
+    "#00b451",
+    "#f7be00",
+    "#ff7818",
+    "#ee3124"
   ];
 
   for (var i = 0; i < sales.length; i++) {
